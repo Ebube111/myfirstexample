52c3437927a43483c6f94830932cd776a9095c06
Ebube Agwaze <ebubeagwaze@gmail.com>
2019-02-12 18:59:41 -0800

    intial commit :wq

--- script/main.js
+++ script/main.js
@@ -1,3 +1,7 @@
-$(function () {
-	$('#panel1').slideUp(300);
+var numOne = document.getElementById("num-one");
+var numTwo = document.getElementById("num-two");
+var addSum = document.getElementById("add-sum");
+
+numOne.addEventListener("click", function() {
+	console.log('hi');
 });
